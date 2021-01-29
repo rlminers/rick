2df8f76ffb9073cb6eff8d7d66c00347f9d0321f
rlminers <rlminers@gmail.com>
2021-01-28 21:52:12 -0600

    updates

---
+++ sql/gluent/gluent_uninstall.sql
@@ -0,0 +1,9 @@
+
+drop role GLUENT_OFFLOAD_SQLMON_ROLE;
+drop role GLUENT_OFFLOAD_REPO_ROLE;
+drop role GLUENT_OFFLOAD_ROLE;
+
+drop user gluent_adm cascade;
+drop user gluent_app cascade;
+drop user gluent_repo cascade;
+
